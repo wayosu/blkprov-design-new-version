2c29f32aeff0b419026f802f24846df9010d5d82
wayosu <wahyusman10@gmail.com>
2021-12-27 05:49:56 +0800

    update responsive design

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -25,3 +25,34 @@ $('.home-slider').owlCarousel({
         }
     }
 });
+
+$('.kejuruan-carousel').owlCarousel({
+    loop: true,
+    autplay: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: true
+        }
+    }
+});
+
+// Typed JS Initialization
+new Typed('#typed', {
+    strings: ['Kompeten'],
+    loop: true,
+    backDelay: 900,
+    backSpeed: 80,
+    typeSpeed: 200,
+    showCursor: false,
+});
